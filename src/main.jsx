384c80a158effe40e921f0e5a3708a516f8c852a
Gean Almeida <gomesgean81@gmail.com>
2024-05-18 15:22:03 -0300

    Fist commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BuscarDeFilmes from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscarDeFilmes />
+  </React.StrictMode>,
+)
